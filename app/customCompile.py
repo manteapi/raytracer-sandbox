7d6ee2fb45882db6d4453c54e9e8597565fae34d
Pierre-Luc Manteaux <pierre-luc.manteaux@inria.fr>
2017-02-22 13:06:54 +0100

    [UPDATE] compile path

--- app/customCompile.py
+++ app/customCompile.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
+COMPILE_FLAGS=""
 PREFIX_PATH="-DCMAKE_PREFIX_PATH=/opt/Qt/5.8/gcc_64/lib/cmake/Qt5"
-
 RAYTRACER_SANDBOX_INCLUDE_DIRS="-DRAYTRACER_SANDBOX_INCLUDE_DIRS=./../../raytracer-sandbox/include"
-RAYTRACER_SANDBOX_LIBRARIES="-DRAYTRACER_SANDBOX_LIBRARIES=./../../raytracer-sandbox/build/libRAYTRACER_SANDBOX.a"
+RAYTRACER_SANDBOX_LIBRARIES="-DRAYTRACER_SANDBOX_LIBRARIES=./../../raytracer-sandbox/buildRelease/libRAYTRACER_SANDBOX.a"
 
 if len(sys.argv)==2:
     if sys.argv[1]=="all":
-        os.system("mkdir -p build && cd build && cmake "+PREFIX_PATH+" "+RAYTRACER_SANDBOX_INCLUDE_DIRS+" "+RAYTRACER_SANDBOX_LIBRARIES+" .. && make -j 8")
+        os.system("mkdir -p build && cd build && cmake "+PREFIX_PATH+" "+COMPILE_FLAGS+" "+RAYTRACER_SANDBOX_INCLUDE_DIRS+" "+RAYTRACER_SANDBOX_LIBRARIES+" .. && make -j 8")
     elif sys.argv[1]=="clean":
         os.system("rm -r build/")
 else :
