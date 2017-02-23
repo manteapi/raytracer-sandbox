2a40900df3435c5e6819283fec23cdb1a1154d5a
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2017-02-23 22:19:00 +0100

    [FIX] Simplify test process

--- script/run-tests.sh
+++ script/run-tests.sh
@@ -1,43 +1,9 @@
 #!/bin/sh
 
-
-#If COMPILER is unset or set to empty string
-#Then we are certainly running this script from home
-#Otherwise it is certainly running through Travis CI
-#In the first case the debug case is sufficient
-#In the second case there is no need to update coverage
-if [ -z "${COMPILER}" ]; then
-    #Debug compilation, testing
-    cd raytracer-sandbox
-    mkdir -p buildDebug
-    cd buildDebug
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
-    make
-    make coverage-gen
-    
-    #Back to root
-    cd ../../
-    
-    #Updating coverage
-    cd script/
-    python gencoveragepng.py
-fi
-
 #Release compilation and testing
 cd raytracer-sandbox
 mkdir -p buildRelease
 cd buildRelease
 cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_BUILD_TYPE=Release ..
-make
-make detailed_test
-
-#Back to root
-cd ../../
-
-#Debug compilation, testing
-cd raytracer-sandbox
-mkdir -p buildDebug
-cd buildDebug
-cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_BUILD_TYPE=Debug ..
 make
 make detailed_test
