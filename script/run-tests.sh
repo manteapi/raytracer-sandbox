892225677acc3cf5e6c358b24f0813d23420cd76
Pierre-Luc Manteaux <pierreluc.manteaux@gmail.com>
2017-02-23 20:05:34 +0100

    [Update] Change coverage custom tool

--- script/run-tests.sh
+++ script/run-tests.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+
+#If COMPILER is unset or set to empty string
+#Then we are certainly running this script from home
+#Otherwise it is certainly running through Travis CI
+#In the first case the debug case is sufficient
+#In the second case there is no need to update coverage
+if [ -z "${COMPILER}" ]; then
+    #Debug compilation, testing
+    cd raytracer-sandbox
+    mkdir -p buildDebug
+    cd buildDebug
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    make
+    make test
+    
+    #Back to root
+    cd ../../
+    
+    #Updating coverage
+    cd script/
+    python gencoveragepng.py
+fi
+
 #Release compilation and testing
 cd raytracer-sandbox
 mkdir -p buildRelease
@@ -18,10 +41,3 @@ cd buildDebug
 cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_BUILD_TYPE=Debug ..
 make
 make test
-
-#Back to root
-cd ../../
-
-#Updating coverage
-cd script/
-python gencoveragepng.py
